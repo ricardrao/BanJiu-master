014c7f66827685b012386e8b2a17b55184e502b7
河海林俊杰 <1113099628@qq.com>
2023-05-03 21:09:46 +0800

    ApiResponse 返回值内容规范

--- src/main/java/com/tencent/wxcloudrun/config/ApiResponse.java
+++ src/main/java/com/tencent/wxcloudrun/config/ApiResponse.java
@@ -9,25 +9,29 @@ import java.util.HashMap;
 public final class ApiResponse {
 
   private Integer code;
-  private String errorMsg;
+  private String message;
   private JSONObject data;
 
-  private ApiResponse(int code, String errorMsg, JSONObject data) {
+  private ApiResponse(int code, String message, JSONObject data) {
     this.code = code;
-    this.errorMsg = errorMsg;
+    this.message = message;
     this.data = data;
   }
   
   public static ApiResponse ok() {
-    return new ApiResponse(0, "", new JSONObject());
+    return new ApiResponse(200, "success", new JSONObject());
   }
 
   public static ApiResponse ok(JSONObject data) {
-    return new ApiResponse(0, "", data);
+    return new ApiResponse(200, "success", data);
   }
 
   public static ApiResponse error(String errorMsg) {
-    return new ApiResponse(0, errorMsg, new JSONObject());
+    return new ApiResponse(400, errorMsg, new JSONObject());
+  }
+
+  public static ApiResponse error(String errorMsg, JSONObject data) {
+    return new ApiResponse(400, errorMsg, data);
   }
 
 }
